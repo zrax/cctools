819841b43231c4ffb06a2e35767e4e00c5dd947a
Michael Hansen <zrax0111@gmail.com>
2010-08-09 21:31:38 -0700

    Added Chips.exe hack database

---
+++ src/ChipsHax.cpp
@@ -0,0 +1,190 @@
+/******************************************************************************
+ * This file is part of CCTools.                                              *
+ *                                                                            *
+ * CCTools is free software: you can redistribute it and/or modify            *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * CCTools is distributed in the hope that it will be useful,                 *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with CCTools.  If not, see <http://www.gnu.org/licenses/>.           *
+ ******************************************************************************/
+
+#include "ChipsHax.h"
+
+void ccl::ChipsHax::set_LastLevel(int level)
+{
+    uint16_t levelValue = level;
+
+    m_stream->seek(0x91C0, SEEK_SET);       // Test for skipping help
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+    m_stream->seek(0xBA14, SEEK_SET);       // Test for endgame dialog
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+    m_stream->seek(0xBB1C, SEEK_SET);       // Test for game state
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+}
+
+void ccl::ChipsHax::set_FakeLastLevel(int level)
+{
+    uint16_t levelValue = level;
+
+    m_stream->seek(0x91B9, SEEK_SET);       // Test for skipping help
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+    m_stream->seek(0xBB14, SEEK_SET);       // Test for game state
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+
+    // Checks for skipping to first level beyond fake last
+    ++levelValue;
+    m_stream->seek(0x9F85, SEEK_SET);
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+    m_stream->seek(0xA6D9, SEEK_SET);
+    m_stream->write(&levelValue, sizeof(uint16_t), 1);
+}
+
+int ccl::ChipsHax::get_LastLevel()
+{
+    uint16_t levelValue;
+    m_stream->seek(0xBB1C, SEEK_SET);
+    m_stream->read(&levelValue, sizeof(uint16_t), 1);
+    return (int)levelValue;
+}
+
+int ccl::ChipsHax::get_FakeLastLevel()
+{
+    uint16_t levelValue;
+    m_stream->seek(0xBB14, SEEK_SET);
+    m_stream->read(&levelValue, sizeof(uint16_t), 1);
+    return (int)levelValue;
+}
+
+
+#define CCPATCH_SIZE    82
+#define CCPATCH_ADDR    0x7C93
+
+static uint8_t ccpatch_orig[] = {
+    //cseg03:1A93
+    0x8A, 0xD8,                 // mov      bl, al
+    0x2A, 0xFF,                 // sub      bh, bh
+    0x8B, 0xC3,                 // mov      ax, bx
+    0xD1, 0xE3,                 // shl      bx, 1
+    0x03, 0xD8,                 // add      bx, ax
+    0xD1, 0xE3,                 // shl      bx, 1
+    0x8D, 0x7E, 0xF6,           // lea      di, [bp-local_A]
+    0x8D, 0xB7, 0x6C, 0x06,     // lea      si, [bx+066Ch]
+    0x8C, 0xD0,                 // mov      ax, ss
+    0x8E, 0xC0,                 // mov      es, ax
+    0xA5,                       // movsw
+    0xA5,                       // movsw
+    0xA5,                       // movsw
+    0x8A, 0x46, 0xF7,           // mov      al, byte ptr [bp-local_9]
+    0x2A, 0xE4,                 // sub      ah, ah
+    0x8B, 0x5E, 0x0E,           // mov      bx, [bp+arg_8]
+    0x89, 0x07,                 // mov      [bx], ax
+    0x80, 0x7E, 0xF6, 0x01,     // cmp      byte ptr [bp-local_A], 1
+    0x75, 0x07,                 // jnz      short cseg03:1AC4
+    0xB8, 0x01, 0x00,           // mov      ax, 1
+    0xE9, 0xD8, 0x01,           // jmp      cseg03:1C9B
+    0x90,                       // nop
+
+    //cseg03:1AC4
+    0x80, 0x7E, 0xF6, 0x02,     // cmp      byte ptr [bp-local_A], 2
+    0x74, 0x03,                 // jz       short cseg03:1ACD
+    0xE9, 0xC7, 0x01,           // jmp      cseg03:1C94
+
+    //cseg03:1ACD
+    0x80, 0x7E, 0xFD, 0x40,     // cmp      [bp-local_3], 40h   ; TILE_BUG_N
+    0x72, 0x09,                 // jb       short cseg03:1ADC
+    0x80, 0x7E, 0xFD, 0x6F,     // cmp      [bp-local_3], 6Fh   ; TILE_PLAYER_E
+    0x77, 0x03,                 // ja       short cseg03:1ADC
+    0xE9, 0x98, 0x01,           // jmp      cseg03:1C74
+
+    //cseg03:1ADC
+    0x80, 0x7E, 0xFD, 0x0A,     // cmp      [bp-local_3], 0Ah   ; TILE_BLOCK
+    0x75, 0x03,                 // jnz      short cseg03:1AE5
+    0xE9, 0x8F, 0x01,           // jmp      cseg03:1C74
+
+    //cseg03:1AE5
+};
+
+static uint8_t ccpatch_patch[] = {
+    //cseg03:1A93
+    0xB3, 0x06,                 // mov      bl, 6
+    0xF6, 0xE3,                 // mul      bl
+    0x89, 0xC3,                 // mov      bx, ax
+    0x8D, 0x7E, 0xF6,           // lea      di, [bp-local_A]
+    0x8D, 0xB7, 0x6C, 0x06,     // lea      si, [bx+66Ch]
+    0x8C, 0xD0,                 // mov      ax, ss
+    0x8E, 0xC0,                 // mov      es, ax
+    0xA5,                       // movsw
+    0xA5,                       // movsw
+    0xA5,                       // movsw
+    0x8A, 0x46, 0xF7,           // mov      al, [bp-local_9]
+    0x28, 0xE4,                 // sub      ah, ah
+    0x8B, 0x5E, 0x0E,           // mov      bx, [bp+arg_8]
+    0x89, 0x07,                 // mov      [bx], ax
+    0x80, 0x7E, 0xF6, 0x01,     // cmp      [bp-local_A], 1
+    0x75, 0x08,                 // jnz      short cseg03:1ABF
+
+    //cseg03:1AB7
+    0xB8, 0x01, 0x00,           // mov      ax, 1
+    0xE9, 0xDE, 0x01,           // jmp      cseg03:1C9B
+
+    //cseg03:1ABD
+    0xEB, 0xF8,                 // jmp      short cseg03:1AB7
+
+    //cseg03:1ABF
+    0x80, 0x7E, 0xF6, 0x02,     // cmp      [bp-local_A], 2
+    0x74, 0x03,                 // jz       short cseg03:1AC8
+    0xE9, 0xCC, 0x01,           // jmp      cseg03:1C94
+
+    //cseg03:1AC8
+    0x80, 0x7E, 0xFD, 0x40,     // cmp      [bp-local_3], 40h   ; TILE_BUG_N
+    0x72, 0x0F,                 // jb       short cseg03:1ADD
+    0x80, 0x7E, 0xFD, 0x6F,     // cmp      [bp-local_3], 6Fh   ; TILE_PLAYER_E
+    0x77, 0x09,                 // jb       short cseg03:1ADD
+
+    //cseg03:1AD4
+    0x83, 0x7E, 0x12, 0x00,     // cmp      [bp+arg_C], 0
+    0x74, 0xDD,                 // jz       short cseg03:1AB7
+    0xE9, 0x97, 0x01,           // jmp      cseg03:1C74
+
+    //cseg03:1ADD
+    0x80, 0x7E, 0xFD, 0x0A,     // cmp      [bp-local_3], 0Ah   ; TILE_BLOCK
+    0x75, 0x02,                 // jnz      short cseg03:1AE5
+    0xEB, 0xEF,                 // jmp      short cseg03:1AD4
+
+    //cseg03:1AE5
+};
+
+void ccl::ChipsHax::set_CCPatch(CCPatchState state)
+{
+    m_stream->seek(CCPATCH_ADDR, SEEK_SET);
+    switch (state) {
+    case CCPatchOriginal:
+        m_stream->write(ccpatch_orig, 1, CCPATCH_SIZE);
+        break;
+    case CCPatchPatched:
+        m_stream->write(ccpatch_patch, 1, CCPATCH_SIZE);
+        break;
+    default:
+        throw ccl::Exception("Invalid patch state parameter");
+    }
+}
+
+ccl::CCPatchState ccl::ChipsHax::get_CCPatch()
+{
+    uint8_t buffer[CCPATCH_SIZE];
+    m_stream->seek(CCPATCH_ADDR, SEEK_SET);
+    m_stream->read(buffer, 1, CCPATCH_SIZE);
+
+    if (memcmp(buffer, ccpatch_orig, CCPATCH_SIZE) == 0)
+        return CCPatchOriginal;
+    if (memcmp(buffer, ccpatch_patch, CCPATCH_SIZE) == 0)
+        return CCPatchPatched;
+    return CCPatchOther;
+}
---
+++ src/ChipsHax.h
@@ -0,0 +1,116 @@
+/******************************************************************************
+ * This file is part of CCTools.                                              *
+ *                                                                            *
+ * CCTools is free software: you can redistribute it and/or modify            *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * CCTools is distributed in the hope that it will be useful,                 *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with CCTools.  If not, see <http://www.gnu.org/licenses/>.           *
+ ******************************************************************************/
+
+#ifndef _CHIPSHAX_H
+#define _CHIPSHAX_H
+
+#include <list>
+#include <string>
+#include <cstring>
+#include "Stream.h"
+
+#define HACK_BOOL16(name, addr) \
+    void set_##name(bool b) \
+    { \
+        uint16_t value = b ? 1 : 0; \
+        m_stream->seek(addr, SEEK_SET); \
+        m_stream->write(&value, sizeof(uint16_t), 1); \
+    } \
+    bool get_##name() \
+    { \
+        uint16_t value; \
+        m_stream->seek(addr, SEEK_SET); \
+        m_stream->read(&value, sizeof(uint16_t), 1); \
+        return (value != 0); \
+    }
+
+#define HACK_STRING(name, addr, maxLength) \
+    void set_##name(const std::string& str) \
+    { \
+        char value[maxLength+1]; \
+        strncpy(value, str.c_str(), maxLength); \
+        value[maxLength] = 0; \
+        m_stream->seek(addr, SEEK_SET); \
+        m_stream->write(value, 1, maxLength+1); \
+    } \
+    std::string get_##name() \
+    { \
+        char value[maxLength+1]; \
+        m_stream->seek(addr, SEEK_SET); \
+        m_stream->read(value, 1, maxLength); \
+        value[maxLength] = 0; \
+        return value; \
+    }
+
+namespace ccl {
+
+enum CCPatchState { CCPatchOriginal, CCPatchPatched, CCPatchOther };
+
+class ChipsHax {
+public:
+    ChipsHax() : m_stream(0) { }
+    void open(ccl::Stream* stream) { m_stream = stream; }
+
+    /* Enter all available hacks here */
+    HACK_BOOL16(IgnorePasswords,    0x482E      )
+    HACK_STRING(DialogTitle,        0x4868,   17)   // Used for message boxes
+    HACK_STRING(FireDeathMsg,       0x4976,   49)
+    HACK_STRING(WaterDeathMsg,      0x49A8,   41)
+    HACK_STRING(BombDeathMsg,       0x49D2,   29)
+    HACK_STRING(BlockDeathMsg,      0x49F0,   35)
+    HACK_STRING(CreatureDeathMsg,   0x4A14,   31)
+    HACK_STRING(TimeLimitMsg,       0x4A34,   19)
+    HACK_STRING(IniFilename,        0x4A68,   11)
+    HACK_STRING(IniEntryName,       0x4A74,   17)
+    HACK_STRING(DataFilename,       0x4AD4,    9)
+    HACK_STRING(WindowTitle,        0x4D16,   16)   // Displayed in main window
+    HACK_STRING(DefaultDeath,       0x4D7C,    6)
+    HACK_STRING(CorruptFileMsg,     0x5166,   39)
+    HACK_STRING(FirstTryMsg,        0x5334,   18)   // Yowser!
+    HACK_STRING(ThirdTryMsg,        0x5347,   14)   // Go Bit Buster!
+    HACK_STRING(FifthTryMsg,        0x5356,   20)   // Finished!  Good work!
+    HACK_STRING(FinalTryMsg,        0x536B,   20)   // At last!  You did it!
+    HACK_STRING(EndgameMsg1,        0x546E,   57)
+    HACK_STRING(EndgameMsg2,        0x54A8,  155)
+    HACK_STRING(ProgressMsg1,       0x56D6,  129)
+    HACK_STRING(ProgressMsg2,       0x5758,  114)
+    HACK_STRING(ProgressMsg3,       0x57CB,  121)
+    HACK_STRING(ProgressMsg4,       0x5845,  123)
+    HACK_STRING(ProgressMsg5,       0x58C1,  120)
+    HACK_STRING(ProgressMsg6,       0x593A,  114)
+    HACK_STRING(ProgressMsg7,       0x59AD,  125)
+    HACK_STRING(ProgressMsg8,       0x5A2B,  101)
+    HACK_STRING(ProgressMsg9,       0x5A91,  131)
+    HACK_STRING(ProgressMsg10,      0x5B15,   60)
+
+    void set_LastLevel(int level);
+    void set_FakeLastLevel(int level);
+    int get_LastLevel();
+    int get_FakeLastLevel();
+
+    // CCExplore's patch for a crash when chip enters a square occupied
+    // by two masked objects (e.g. monsters and keys)
+    void set_CCPatch(CCPatchState state);
+    CCPatchState get_CCPatch();
+
+private:
+    ccl::Stream* m_stream;
+};
+
+}
+
+#endif
