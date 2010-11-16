d795bd58a343515e0dfccdccf5741b0db5b05198
Michael Hansen <zrax0111@gmail.com>
2010-11-15 16:13:41 -0800

    Clean up tileset generator script

--- res/gen_tilesets.py
+++ res/gen_tilesets.py
@@ -2,49 +2,33 @@
 
 import struct
 
-name = 'TileWorld/Editor 32x32'
-desc = 'Default 32x32 TileWorld Editor Graphics'
+def write_tileset(filename, name, desc, base_gfx_file, overlay_gfx_file):
+    src = open(base_gfx_file, 'rb')
+    base_data = src.read()
+    src.close()
+    src = open(overlay_gfx_file, 'rb')
+    overlay_data = src.read()
+    src.close()
 
-src = open('TW32_base.png', 'rb')
-base_data = src.read()
-src.close()
-src = open('TW32_overlay.png', 'rb')
-overlay_data = src.read()
-src.close()
+    tis = open(filename, 'wb')
+    tis.write('CCTILE01')
+    tis.write(struct.pack('I', len(name)))
+    tis.write(name)
+    tis.write(struct.pack('I', len(desc)))
+    tis.write(desc)
+    tis.write(struct.pack('B', 32))
+    tis.write(struct.pack('I', len(base_data)))
+    tis.write(base_data)
+    tis.write(struct.pack('I', len(overlay_data)))
+    tis.write(overlay_data)
+    tis.close()
 
-tis = open('TW32.tis', 'wb')
-tis.write('CCTILE01')
-tis.write(struct.pack('I', len(name)))
-tis.write(name)
-tis.write(struct.pack('I', len(desc)))
-tis.write(desc)
-tis.write(struct.pack('B', 32))
-tis.write(struct.pack('I', len(base_data)))
-tis.write(base_data)
-tis.write(struct.pack('I', len(overlay_data)))
-tis.write(overlay_data)
-tis.close()
+# Generate default tilesets if called from the command line
+if __name__ == '__main__':
+    write_tileset('TW32.tis', 'TileWorld/Editor 32x32',
+                'Default 32x32 TileWorld Editor Graphics',
+                'TW32_base.png', 'TW32_overlay.png')
 
-
-name = 'MSCC/Editor Color'
-desc = 'Microsoft WEP Default 32x32 Editor Graphics'
-
-src = open('MSCC_base.png', 'rb')
-base_data = src.read()
-src.close()
-src = open('MSCC_overlay.png', 'rb')
-overlay_data = src.read()
-src.close()
-
-tis = open('WEP.tis', 'wb')
-tis.write('CCTILE01')
-tis.write(struct.pack('I', len(name)))
-tis.write(name)
-tis.write(struct.pack('I', len(desc)))
-tis.write(desc)
-tis.write(struct.pack('B', 32))
-tis.write(struct.pack('I', len(base_data)))
-tis.write(base_data)
-tis.write(struct.pack('I', len(overlay_data)))
-tis.write(overlay_data)
-tis.close()
+    write_tileset('WEP.tis', 'MSCC/Editor Color',
+                'Microsoft WEP Default 32x32 Editor Graphics',
+                'MSCC_base.png', 'MSCC_overlay.png')
