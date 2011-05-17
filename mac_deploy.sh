b5670af43fd69990218f8b03d3f91c882aa87ce7
Michael Hansen <zrax0111@gmail.com>
2011-05-17 16:43:27 -0700

    Change Mac build process for creating a .pkg file instead of raw .apps

---
+++ mac_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+    echo "Usage: $0 /path/to/dist_files"
+    exit 1
+fi
+DISTROOT=`cd $1; pwd`
+
+macdeployqt $DISTROOT/CCEdit.app
+macdeployqt $DISTROOT/CCPlay.app
+
+# Symlink the other packages to save space
+#cd $DISTROOT/CCPlay.app
+#rm -r Contents/Frameworks Contents/PlugIns
+#ln -s ../CCEdit.app/Contents/Frameworks Contents/Frameworks
+#ln -s ../CCEdit.app/Contents/PlugIns Contents/PlugIns
+
+# Move everything to the right places for the packager
+rm -rf $DISTROOT/Package_Root
+mkdir -p $DISTROOT/Package_Root/Applications
+mkdir -p $DISTROOT/Package_Root/usr
+mv $DISTROOT/CCEdit.app $DISTROOT/Package_Root/Applications/
+mv $DISTROOT/CCPlay.app $DISTROOT/Package_Root/Applications/
+mv $DISTROOT/share $DISTROOT/Package_Root/usr/
