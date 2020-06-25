7e6ea22cd405fc942eba6b669a4612b1270eaf0b
Michael Hansen <zrax0111@gmail.com>
2020-06-25 16:28:21 -0700

    Show allowable ranges in the resize dialog.

--- src/CC2Edit/ResizeDialog.cpp
+++ src/CC2Edit/ResizeDialog.cpp
@@ -27,7 +27,8 @@ ResizeDialog::ResizeDialog(const QSize& curSize, QWidget* parent)
 {
     setWindowTitle(tr("Resize Map"));
 
-    auto curSizeLabel = new QLabel(tr("Current Size:  %1 x %2")
+    auto curSizeLabel = new QLabel(tr("Current Size:"), this);
+    auto curSizeValue = new QLabel(tr("%1 x %2")
             .arg(curSize.width()).arg(curSize.height()), this);
 
     auto widthLabel = new QLabel(tr("New &Width: "), this);
@@ -35,12 +36,14 @@ ResizeDialog::ResizeDialog(const QSize& curSize, QWidget* parent)
     m_width->setRange(10, 100);
     m_width->setValue(curSize.width());
     widthLabel->setBuddy(m_width);
+    auto widthRangeLabel = new QLabel(tr("(10 - 100)"), this);
 
     auto heightLabel = new QLabel(tr("New &Height:"), this);
     m_height = new QSpinBox(this);
     m_height->setRange(10, 100);
     m_height->setValue(curSize.height());
     heightLabel->setBuddy(m_height);
+    auto heightRangeLabel = new QLabel(tr("(10 - 100)"), this);
 
     auto buttons = new QDialogButtonBox(
             QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
@@ -50,12 +53,15 @@ ResizeDialog::ResizeDialog(const QSize& curSize, QWidget* parent)
     layout->setContentsMargins(8, 8, 8, 8);
     layout->setVerticalSpacing(8);
     layout->setHorizontalSpacing(8);
-    layout->addWidget(curSizeLabel, 0, 0, 1, 2);
+    layout->addWidget(curSizeLabel, 0, 0);
+    layout->addWidget(curSizeValue, 0, 1);
     layout->addWidget(widthLabel, 1, 0);
     layout->addWidget(m_width, 1, 1);
+    layout->addWidget(widthRangeLabel, 1, 2);
     layout->addWidget(heightLabel, 2, 0);
     layout->addWidget(m_height, 2, 1);
-    layout->addWidget(buttons, 3, 0, 1, 2);
+    layout->addWidget(heightRangeLabel, 2, 2);
+    layout->addWidget(buttons, 3, 0, 1, 3);
 
     connect(buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
     connect(buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
