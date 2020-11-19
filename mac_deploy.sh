e5c7224fa5f8b61b06e147b20c2c17026d3d203b
Michael Hansen <zrax0111@gmail.com>
2020-11-19 11:04:48 -0800

    Update mac_deploy.sh to work correctly with newer versions of Qt5

--- mac_deploy.sh
+++ mac_deploy.sh
@@ -4,23 +4,20 @@ if [[ "$1" == "" ]]; then
     echo "Usage: $0 /path/to/dist_files"
     exit 1
 fi
-DISTROOT=`cd $1; pwd`
+cd "$1"
 
-macdeployqt $DISTROOT/CCEdit.app
-macdeployqt $DISTROOT/CCPlay.app
-macdeployqt $DISTROOT/CCHack.app
+macdeployqt CCEdit.app -executable=CCEdit.app/Contents/MacOS/CCEdit
+macdeployqt CC2Edit.app -executable=CC2Edit.app/Contents/MacOS/CC2Edit
+macdeployqt CCPlay.app -executable=CCPlay.app/Contents/MacOS/CCPlay
+macdeployqt CCHack.app -executable=CCHack.app/Contents/MacOS/CCHack
+
+cp -r share CCEdit.app/Contents/
+cp -r share CC2Edit.app/Contents/
+rm -r share
+cp ../LICENSE* .
 
 # Symlink the other packages to save space
 #cd $DISTROOT/CCPlay.app
 #rm -r Contents/Frameworks Contents/PlugIns
 #ln -s ../CCEdit.app/Contents/Frameworks Contents/Frameworks
 #ln -s ../CCEdit.app/Contents/PlugIns Contents/PlugIns
-
-# Move everything to the right places for the packager
-rm -rf $DISTROOT/Package_Root
-mkdir -p $DISTROOT/Package_Root/Applications
-mkdir -p $DISTROOT/Package_Root/usr
-mv $DISTROOT/CCEdit.app $DISTROOT/Package_Root/Applications/
-mv $DISTROOT/CCPlay.app $DISTROOT/Package_Root/Applications/
-mv $DISTROOT/CCHack.app $DISTROOT/Package_Root/Applications/
-mv $DISTROOT/share $DISTROOT/Package_Root/usr/
